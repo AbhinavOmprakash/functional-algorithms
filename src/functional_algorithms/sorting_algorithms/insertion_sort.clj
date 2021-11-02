ba22bd71092f42d931d04dfdb8c860871343e33a
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-11-02 22:10:59 +0530

    Add insertion-sort and tests

---
+++ src/functional_algorithms/sorting_algorithms/insertion_sort.clj
@@ -0,0 +1,8 @@
+(ns functional_algorithms.sorting_algorithms.insertion_sort)
+
+(defn insert [coll x]
+  (let [[left right] (split-with #(< % x) coll)]
+    (concat left [x] right))) 
+
+(defn sort [coll]
+  (reduce insert [] coll))
--- test/functional_algorithms/sorting_algorithms/sort_test.clj
+++ test/functional_algorithms/sorting_algorithms/sort_test.clj
@@ -1,8 +1,10 @@
 (ns functional_algorithms.sorting_algorithms.sort_test
   (:require [clojure.test :refer :all]
-            [functional_algorithms.sorting_algorithms.simple_sort :as simple]))
+            [functional_algorithms.sorting_algorithms.simple_sort :as simple]
+            [functional_algorithms.sorting_algorithms.insertion_sort :as insertion]))
 
 (deftest sort
   (let [test-data (repeatedly 10 #(rand-int 100))
         expected (clojure.core/sort test-data)]
-    (is (= expected (simple/sort test-data)))))
+    (is (= expected (simple/sort test-data)))
+    (is (= expected (insertion/sort test-data)))))
