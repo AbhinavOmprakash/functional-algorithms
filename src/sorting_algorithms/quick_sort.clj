69ad9a9f7385e539bcc90ed72d631d2aad12ef13
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-11-03 09:22:35 +0530

    Add quicksort and tests

--- src/sorting_algorithms/quick_sort.clj
+++ src/sorting_algorithms/quick_sort.clj
@@ -1,2 +1,15 @@
 (ns sorting-algorithms.quick-sort)
 
+(defn- remove-at [nth coll]
+  (let [[left right] (split-at (- nth 1) coll)]
+    (concat left (rest right))))
+
+(defn sort [coll]
+  (if (< (count coll) 2)
+    coll
+    (let [mid (/ (count coll) 2)
+          [pivot] (drop (- mid 1) coll)
+          coll* (remove-at mid coll)]
+      (lazy-cat (sort (filter #(<= % pivot)coll*) )
+                [pivot]
+                (sort (filter #(> % pivot)coll*))))))
--- test/sorting_algorithms/sort_test.clj
+++ test/sorting_algorithms/sort_test.clj
@@ -1,10 +1,12 @@
 (ns sorting_algorithms.sort_test
   (:require [clojure.test :refer :all]
             [sorting-algorithms.selection-sort :as selection]
-            [sorting-algorithms.insertion-sort :as insertion]))
+            [sorting-algorithms.insertion-sort :as insertion]
+            [sorting-algorithms.quick-sort :as quick]))
 
 (deftest sort
   (let [test-data (repeatedly 10 #(rand-int 100))
         expected (clojure.core/sort test-data)]
     (is (= expected (selection/sort test-data)))
-    (is (= expected (insertion/sort test-data)))))
+    (is (= expected (insertion/sort test-data)))
+    (is (= expected (quick/sort test-data)))))
