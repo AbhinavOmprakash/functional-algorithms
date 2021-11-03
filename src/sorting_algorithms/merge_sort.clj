ed55b122e49176ddafecd933efb3ff83671f8e65
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-11-03 14:28:25 +0530

    Add merge-sort with tests

---
+++ src/sorting_algorithms/merge_sort.clj
@@ -0,0 +1,30 @@
+(ns sorting-algorithms.merge-sort)
+
+(defn- merge
+  ([coll]coll)
+  ([coll1 coll2] (merge coll1 coll2 []))
+  ([coll1 coll2 result]
+   (if (some empty? [coll1 coll2])
+         (lazy-cat result coll1 coll2)
+         (let [[f1] coll1
+               [f2] coll2]
+           (if (<= f1 f2)
+             (recur (rest coll1) coll2 (lazy-cat result [f1]))
+             (recur coll1 (rest coll2) (lazy-cat result [f2])))))))
+
+(defn- sort-helper [coll]
+; if its a nested list of one element that means 
+; all elements have been merged and sorted. 
+; flatten  
+  (if (= (count coll) 1) 
+    (flatten coll)
+    (->> coll
+         (partition 2 2 nil)
+         (map #(apply merge %))
+         (recur))))
+
+;; Bottom up merge sort.
+(defn sort
+  ([coll]
+   ;; start with a seq of 1-element vectors
+   (sort-helper (map vector coll))))
--- test/sorting_algorithms/sort_test.clj
+++ test/sorting_algorithms/sort_test.clj
@@ -2,11 +2,13 @@
   (:require [clojure.test :refer :all]
             [sorting-algorithms.selection-sort :as selection]
             [sorting-algorithms.insertion-sort :as insertion]
-            [sorting-algorithms.quick-sort :as quick]))
+            [sorting-algorithms.quick-sort :as quick]
+            [sorting-algorithms.merge-sort :as merge]))
 
 (deftest sort
   (let [test-data (repeatedly 10 #(rand-int 100))
         expected (clojure.core/sort test-data)]
     (is (= expected (selection/sort test-data)))
     (is (= expected (insertion/sort test-data)))
-    (is (= expected (quick/sort test-data)))))
+    (is (= expected (quick/sort test-data)))
+    (is (= expected (merge/sort test-data)))))
