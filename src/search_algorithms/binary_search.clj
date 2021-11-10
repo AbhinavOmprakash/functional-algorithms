6fb62629a34fc16dc35cf635fd9daf00d7790a86
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-11-10 13:38:53 +0530

    replace drop/take with subvec

--- src/search_algorithms/binary_search.clj
+++ src/search_algorithms/binary_search.clj
@@ -4,18 +4,16 @@
   (when (seq v)
     (if-not (apply <= v)
       (recur (vec (sort  v)) x)
-      (let [mid-idx (quot (count v) 2)
+      (let [count* (count v)
+            mid-idx (quot count* 2)
             mid-el (get v mid-idx)]
         (cond
           (= mid-el x) x
 
-          ; adding 1 to mid-idx is necessary 
-          ; because mid-idx will become 0 at some point and 
-          ; drop 0 will always return a non-empty seq
           (< mid-el x)
-          (recur (vec (drop (+ 1 mid-idx) v)) x)
+          (recur (subvec v (+ 1 mid-idx) count*) x)
 
           (> mid-el x)
-          (recur (vec (take mid-idx v)) x))))))
+          (recur (subvec v 0 mid-idx) x))))))
 
 
